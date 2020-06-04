eeebb14282fb8092a2a341b1d3b0ef97031d7913
danzo03 <62819457+danzo03@users.noreply.github.com>
2020-06-04 18:15:57 +0000

    icons

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(VillesTableSeeder::class);
+        //$this->call(VillesTableSeeder::class);
         $this->call(CommunesTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(SousCateTableSeeder::class);
--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,12 +1,20 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('achats') }}'><i class='nav-icon la la-database'></i> commandes</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('produit') }}'><i class="nav-icon las la-cart-plus"></i> Produits</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class='nav-icon la la-database'></i> Categories</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('villes') }}'><i class='nav-icon la la-database'></i> Villes</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('communes') }}'><i class='nav-icon la la-database'></i> Communes</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('achats') }}'><i class='nav-icon la la-database'></i> Achats</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('souscategorie') }}'><i class='nav-icon la la-database'></i> Souscategories</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('images') }}'><i class='nav-icon la la-database'></i> Images</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('adresse') }}'><i class='nav-icon la la-database'></i> Adresses</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('reduction') }}'><i class='nav-icon la la-database'></i> Reductions</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('souscategorie') }}'><i class='nav-icon la la-database'></i> Sous Categories</a></li>
+
+
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('marque') }}'><i class='nav-icon la la-database'></i> Marques</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('produit') }}'><i class='nav-icon la la-question'></i> Produits</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('reduction') }}'><i class='nav-icon la la-database'></i> Bons</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('villes') }}'><i class=" nav-icon las la-city"></i></i> Villes</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('communes') }}'><i class='nav-icon la la-database'></i> Communes</a></li>
+
+
+<!--li class='nav-item'><a class='nav-link' href='{{ backpack_url('images') }}'>
+<i class='nav-icon la la-database'></i> Images</a></li-->
+<!--li class='nav-item'><a class='nav-link' href='{{ backpack_url('adresse') }}'>
+<i class='nav-icon la la-database'></i> Adresses</a></li-->
+
+
